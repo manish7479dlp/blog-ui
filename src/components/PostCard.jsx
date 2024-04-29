f4bf9f657cfed49ae101180d245998bc3a0c0983
manish7479dlp <manish7479dlp@gmail.com>
2024-04-29 17:14:31 +0530

    refac: imgurl in postCard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { BiSolidRightArrowCircle } from "react-icons/bi";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ReactTimeAgo from "react-time-ago";
 import { FcOvertime } from "react-icons/fc";
 const apiURL = process.env.REACT_APP_APIURL;
-const imgLink = apiURL;
 
 const PostCard = ({ cover, summary, createdAt, _id }) => {
   return (
     <div className="card m-3 p-3" style={{ width: "18rem" }}>
-    <div className="  " >
-    <img
-        src={`${imgLink}/${cover}`}
-        className="card-img-top object-fit-fill rounded"
-        alt="post-img"
-        style={{height: "150px"}}
-       
-      />
-    </div>
-      
+      <div className="  ">
+        <img
+          src={cover}
+          className="card-img-top object-fit-fill rounded"
+          alt="post-img"
+          style={{ height: "150px" }}
+        />
+      </div>
+
       <div className="card-body">
         <p className="text-end">
           <FcOvertime />{" "}
@@ -34,7 +32,7 @@ const PostCard = ({ cover, summary, createdAt, _id }) => {
           <span className="">{summary}</span>
         </p>
         <div className="text-center">
-          <Link to = {"../post/" + _id} className="btn btn-primary">
+          <Link to={"../post/" + _id} className="btn btn-primary">
             Read
           </Link>
         </div>
